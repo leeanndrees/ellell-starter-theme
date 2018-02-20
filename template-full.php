e2cc51391a28aef5dcfb79675cf2b62900ce010e
LeeannDrees <leeann.drees@gmail.com>
2018-02-20 15:16:41 -0500

    moar whitespace

--- template-full.php
+++ template-full.php
@@ -1,20 +1,37 @@
 <?php get_header();
 /* Template Name: Full-Width Page */
 ?>
+
 <div class="container site-content">
+
 <div class="row">
+
   <main class="col-sm-8">
+
     <?php echo "This is template-full.php"; ?>
-    <?php while(have_posts()) : the_post(); ?>
+
+    <?php while( have_posts() ) : the_post(); ?>
+
       <article class="page-content">
-        <?php the_title( '<h2>', '</h2>'); ?>
+
+        <?php the_title( '<h2>', '</h2>' ); ?>
+
         <?php the_content(); ?>
+
       </article>
+
     <?php endwhile; ?>
+
   </main>
+
   <aside class="sidebar-right col-sm-4">
-    <?php dynamic_sidebar('sidebar-right'); ?>
+
+    <?php dynamic_sidebar( 'sidebar-right' ); ?>
+
   </aside>
+
 </div>
+
 </div>
+
 <?php get_footer(); ?>
